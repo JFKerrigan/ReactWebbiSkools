1bc2106f3cb86c05784e8ab2c8ef3093237897a8
JFKerrigan <miss.supernothing@gmail.com>
2022-04-12 13:34:40 +0100

    viewQuizzes state showing the full details

--- client/src/components/ViewQuiz.js
+++ client/src/components/ViewQuiz.js
@@ -1,14 +1,28 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom'
 
 
 
 const ViewQuiz = (props) => {
+    const [questions, setQuestions] = useState({})
     const {quiz} = useParams()
     console.log(quiz)
     useEffect(() => {
         fetch(`http://localhost:1337/api/viewQuizzes/${quiz}`)
-    })
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw response
+        })
+        .then(data => {
+            setQuestions(data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        console.log(questions)
+    }, [])
 
     return (
         <div>
